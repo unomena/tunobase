20bf06ee2cac0d18f7cc479ea3e537ef042cb982
Michael Whelehan <michael@unomena.com>
2013-11-20 10:03:40 +0200

    improved throttling

---
+++ tunobase/commenting/throttling.py
@@ -0,0 +1,27 @@
+'''
+Created on 20 Nov 2013
+
+@author: michael
+'''
+from django.utils import timezone
+
+from tunobase.commenting import models
+
+def check_throttle(user, ip_address, comment_period_lockout, num_comments_allowed_in_lockout):
+    if user is not None:
+        queryset = models.CommentModel.objects.filter(
+            user=user
+        )
+    else:
+        queryset = models.CommentModel.objects.filter(
+            ip_address=ip_address
+        )
+        
+    latest_comment_list = list(
+        queryset.order_by('-publish_at')[:num_comments_allowed_in_lockout]
+    )
+    if len(latest_comment_list) == num_comments_allowed_in_lockout:
+        oldest_comment = latest_comment_list[-1]
+        return oldest_comment.publish_at > timezone.now() - comment_period_lockout
+    
+    return True
---
+++ tunobase/social_media/tunosocial/throttling.py
@@ -0,0 +1,27 @@
+'''
+Created on 20 Nov 2013
+
+@author: michael
+'''
+from django.utils import timezone
+
+from tunobase.social_media.tunosocial import models
+
+def check_throttle(user, ip_address, like_period_lockout, num_likes_allowed_in_lockout):
+    if user is not None:
+        queryset = models.Like.objects.filter(
+            user=user
+        )
+    else:
+        queryset = models.Like.objects.filter(
+            ip_address=ip_address
+        )
+        
+    latest_like_list = list(
+        queryset.order_by('-created_at')[:num_likes_allowed_in_lockout]
+    )
+    if len(latest_like_list) == num_likes_allowed_in_lockout:
+        oldest_like = latest_like_list[-1]
+        return oldest_like.created_at > timezone.now() - like_period_lockout
+    
+    return True
