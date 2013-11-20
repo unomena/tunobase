5d8a98810344c049e130d3e70c73370fc58dc1e5
Michael Whelehan <michael@unomena.com>
2013-11-20 17:26:09 +0200

    made updates to comments

--- tunobase/commenting/templatetags/commenting_widgets.py
+++ tunobase/commenting/templatetags/commenting_widgets.py
@@ -10,6 +10,7 @@ from django import template
 from django.contrib.contenttypes.models import ContentType
 from django.core.paginator import Paginator
 from django.core.urlresolvers import reverse_lazy
+from django.core.exceptions import ImproperlyConfigured
 
 from tunobase.commenting import models
 
@@ -17,7 +18,7 @@ register = template.Library()
 
 @register.inclusion_tag('commenting/inclusion_tags/commenting_widget.html', 
                         takes_context=True)
-def commenting_widget(context, obj, paginate_by=10, page_obj=None):
+def commenting_widget(context, obj, paginate_by=None, page_obj=None):
     context = copy(context)
     content_type_id= ContentType.objects.get_for_model(obj).id
     if page_obj is None:
@@ -25,10 +26,20 @@ def commenting_widget(context, obj, paginate_by=10, page_obj=None):
             content_type_id, 
             obj.pk
         )
-        paginator = Paginator(comments, paginate_by)
-        page = paginator.page(1)
+        if paginate_by is not None:
+            paginator = Paginator(comments, paginate_by)
+            page = paginator.page(1)
+        else:
+            page = None
     else:
+        comments = None
         page = page_obj
+        
+    if comments is None and page is None:
+        raise ImproperlyConfigured(
+            "Attributes 'paginate_by' or 'page_obj' must be set"
+        )
+        
     params = urllib.urlencode({
         'content_type_id': content_type_id, 
         'object_pk': obj.pk,
@@ -38,7 +49,7 @@ def commenting_widget(context, obj, paginate_by=10, page_obj=None):
     context.update({
         'content_type_id': content_type_id,
         'object': obj,
-        'object_list': page,
+        'object_list': page or comments,
         'page_obj': page,
         'load_more_url': '%s?%s' % (reverse_lazy('load_more_comments'), params)
     })
