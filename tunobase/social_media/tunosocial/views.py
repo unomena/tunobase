52a92305bf3602acc4e1e0003553e52e09ef28a6
Michael Whelehan <michael@unomena.com>
2013-11-20 14:52:10 +0200

    made likes progressively enhanced

--- tunobase/social_media/tunosocial/views.py
+++ tunobase/social_media/tunosocial/views.py
@@ -15,14 +15,14 @@ from django.contrib import messages
 from tunobase.core import utils as core_utils, throttling as core_throttling
 from tunobase.social_media.tunosocial import models, exceptions, throttling
 
-def validate(self, user, throttle_key, ip_address):
+def _validate(request, user, throttle_key, ip_address):
     like_period_lockout = getattr(settings, 'LIKE_PERIOD_LOCKOUT', None)
     num_likes_allowed_in_lockout = \
         getattr(settings, 'NUM_LIKES_ALLOWED_IN_PERIOD', 5)
     if like_period_lockout is not None:
-        if core_throttling.check_throttle_exists(self.request, throttle_key):
+        if core_throttling.check_throttle_exists(request, throttle_key):
             throttled = core_throttling.check_throttle(
-                self.request, 
+                request, 
                 throttle_key, 
                 like_period_lockout, 
                 num_likes_allowed_in_lockout
@@ -40,31 +40,26 @@ def validate(self, user, throttle_key, ip_address):
                 'You are liking too quickly. '
                 'Please wait before liking again'
             )
-
-class AddLike(generic_views.View):
-    
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated():
-            user = request.user
-        else:
-            user = None
+            
+def _like(request, action):
+    if request.user.is_authenticated():
+        user = request.user
+    else:
+        user = None
+        
+    if request.method == 'POST':
         content_type_id = request.POST.get('content_type_id', None)
         object_pk = request.POST.get('object_pk', None)
-        ip_address = core_utils.get_client_ip(request)
-        throttle_key = 'liking_%s' % object_pk
+    else:
+        content_type_id = request.GET.get('content_type_id', None)
+        object_pk = request.GET.get('object_pk', None)
         
-        try:
-            self.validate(user, throttle_key, ip_address)
-        except exceptions.RapidLikingError, e:
-            if request.is_ajax():
-                return core_utils.respond_with_json({
-                    'success': False,
-                    'reason': e
-                })
-                
-            messages.error(request, e)
-            return redirect(request.META['HTTP_REFERER'])
-        
+    ip_address = core_utils.get_client_ip(request)
+    throttle_key = 'liking_%s' % object_pk
+    
+    _validate(request, user, throttle_key, ip_address)
+    
+    if action == 'add':
         like = models.Like.objects.create(
             user=user,
             content_type_id=content_type_id,
@@ -74,39 +69,7 @@ class AddLike(generic_views.View):
         )
         
         core_throttling.add_to_throttle(request, throttle_key, like.created_at)
-        
-        if request.is_ajax():
-            return core_utils.respond_with_json({
-                'success': True
-            })
-        
-        messages.success(request, 'You have liked this')
-        return redirect(request.META['HTTP_REFERER'])
-        
-class RemoveLike(generic_views.View):
-
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated():
-            user = request.user
-        else:
-            user = None
-        content_type_id = request.POST.get('content_type_id', None)
-        object_pk = request.POST.get('object_pk', None)
-        ip_address = core_utils.get_client_ip(request)
-        throttle_key = 'liking_%s' % object_pk
-        
-        try:
-            self.validate(user, throttle_key, ip_address)
-        except exceptions.RapidLikingError, e:
-            if request.is_ajax():
-                return core_utils.respond_with_json({
-                    'success': False,
-                    'reason': e
-                })
-            
-            messages.error(request, e)
-            return redirect(request.META['HTTP_REFERER'])
-        
+    elif action =='remove':
         like = get_object_or_404(
             models.Like, 
             user=user,
@@ -114,11 +77,54 @@ class RemoveLike(generic_views.View):
             object_pk=object_pk
         )
         like.delete()
-        
-        if request.is_ajax():
-            return core_utils.respond_with_json({
-                'success': True
-            })
+    
+
+class AddLike(generic_views.View):
+    
+    def get(self, request, *args, **kwargs):
+        try:
+            _like(request, 'add')
+        except exceptions.RapidLikingError, e:
+            messages.error(request, e)
+            return redirect(request.META['HTTP_REFERER'])
         
         messages.success(request, 'You have liked this')
         return redirect(request.META['HTTP_REFERER'])
+    
+    def post(self, request, *args, **kwargs):
+        try:
+            _like(request, 'add')
+        except exceptions.RapidLikingError, e:
+            return core_utils.respond_with_json({
+                'success': False,
+                'reason': e
+            })
+        
+        return core_utils.respond_with_json({
+            'success': True
+        })
+        
+class RemoveLike(generic_views.View):
+    
+    def get(self, request, *args, **kwargs):
+        try:
+            _like(request, 'remove')
+        except exceptions.RapidLikingError, e:
+            messages.error(request, e)
+            return redirect(request.META['HTTP_REFERER'])
+        
+        messages.success(request, 'You have unliked this')
+        return redirect(request.META['HTTP_REFERER'])
+
+    def post(self, request, *args, **kwargs):
+        try:
+            _like(request, 'remove')
+        except exceptions.RapidLikingError, e:
+            return core_utils.respond_with_json({
+                'success': False,
+                'reason': e
+            })
+        
+        return core_utils.respond_with_json({
+            'success': True
+        })
