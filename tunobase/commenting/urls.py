b55e1f4a15e3434ef867f2f22d3b906a166dae00
Christina Maxwell <christina@unomena.com>
2013-12-17 13:51:06 +0200

    PEP8 commenting app urls

--- tunobase/commenting/urls.py
+++ tunobase/commenting/urls.py
@@ -10,7 +10,7 @@ from honeypot.decorators import check_honeypot
 from tunobase.commenting import views, forms
 
 urlpatterns = patterns('',
-                              
+
     url(r'^post-comment/$',
         check_honeypot(views.PostComment.as_view(
             form_class=forms.CommentForm,
@@ -18,12 +18,12 @@ urlpatterns = patterns('',
         )),
         name='post_comment'
     ),
-                       
+
     url(r'^report-comment/(?P<pk>\d+)/$',
         views.ReportComment.as_view(),
         name='report_comment'
     ),
-                       
+
     url(r'^load-more-comments/$',
         views.LoadMoreComments.as_view(
             partial_template_name='commenting/includes/comments.html'
