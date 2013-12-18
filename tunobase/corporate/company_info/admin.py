9f71fff1a328cf14ee3c1d1bda39c49b54bfd8ce
Christina Maxwell <christina@unomena.com>
2013-12-18 15:48:29 +0200

    PEP8: Docstrings for company_info app

--- tunobase/corporate/company_info/admin.py
+++ tunobase/corporate/company_info/admin.py
@@ -1,8 +1,20 @@
-'''
+"""
+COMPANY INFO APP
+
+This module determins which models are displayed in Django's
+admin.
+
+Classes:
+    n/a
+
+Functions:
+    n/a
+
 Created on 23 Oct 2013
 
 @author: michael
-'''
+
+"""
 from django.contrib import admin
 
 from tunobase.corporate.company_info import models
--- tunobase/corporate/company_info/models.py
+++ tunobase/corporate/company_info/models.py
@@ -1,17 +1,29 @@
-'''
+"""
+COMPANY INFO APP
+
+This module provides the model layout for the database.
+
+Classes:
+    Vacancy
+    CompanyMemberPosition
+    CompanyMember
+
+Functions:
+    n/a
+
 Created on 23 Oct 2013
 
 @author: michael
-'''
+
+"""
 from django.db import models
 from django.contrib.sites.models import Site
 
 from tunobase.core import models as core_models
 
 class Vacancy(core_models.ContentModel):
-    '''
-    Job vacancies within the company
-    '''
+    """Job vacancies within the company."""
+
     external_link = models.URLField(blank=True, null=True)
 
     class Meta:
@@ -19,13 +31,14 @@ class Vacancy(core_models.ContentModel):
 
 
 class CompanyMemberPosition(core_models.StateModel, core_models.SlugModel):
-    '''
-    Member positions within the Company
-    '''
+    """Member positions within the Company."""
+
     sites = models.ManyToManyField(Site, blank=True, null=True)
     order = models.PositiveIntegerField(default=0, db_index=True)
 
     def __unicode__(self):
+        """Return a unicode object."""
+
         return u'%s' % self.title
 
     class Meta:
@@ -33,9 +46,8 @@ class CompanyMemberPosition(core_models.StateModel, core_models.SlugModel):
 
 
 class CompanyMember(core_models.ContentModel):
-    '''
-    Members of the company
-    '''
+    """Members of the company."""
+
     default_image_category = 'company_member'
 
     positions = models.ManyToManyField(
