88b409f0549a3680ad8187281325789b7b05321c
Euan Jonker <euan@unomena.com>
2014-03-25 12:57:54 +0200

    Show more information in the company info on admin.

--- tunobase/corporate/company_info/admin.py
+++ tunobase/corporate/company_info/admin.py
@@ -4,21 +4,20 @@ COMPANY INFO APP
 This module determins which models are displayed in Django's
 admin.
 
-Classes:
-    n/a
-
-Functions:
-    n/a
-
-Created on 23 Oct 2013
-
-@author: michael
-
 """
 from django.contrib import admin
 
+from tunobase.core.admin import SiteListAdminMixin
 from tunobase.corporate.company_info import models
 
+class CompanyMemberAdmin(admin.ModelAdmin, SiteListAdminMixin):
+
+    list_display = (
+            'title', 'state', 'slug', 'site_list'
+    )
+    list_filter = ('title', 'state',)
+    search_fields = ('title',)
+
 admin.site.register(models.Vacancy)
-admin.site.register(models.CompanyMemberPosition)
-admin.site.register(models.CompanyMember)
+admin.site.register(models.CompanyMemberPosition, CompanyMemberAdmin)
+admin.site.register(models.CompanyMember, CompanyMemberAdmin)
