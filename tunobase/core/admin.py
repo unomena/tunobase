d7076ef59e31a4fe636825ec738302fb36f33346
Michael Whelehan <michael@unomena.com>
2015-04-22 11:56:43 +0200

    upped version to 1.1.25

--- tunobase/core/admin.py
+++ tunobase/core/admin.py
@@ -9,6 +9,7 @@ from django.contrib import admin
 
 from tunobase.core import models
 
+
 class SiteListAdminMixin(object):
     """Return a list of sites."""
 
@@ -17,24 +18,31 @@ class SiteListAdminMixin(object):
 
         return ', '.join([site.domain for site in model.sites.all()])
 
+
 class ContentModelAdmin(admin.ModelAdmin, SiteListAdminMixin):
     """Determine how the content model is displayed in the admin."""
 
     list_display = (
-            'title', 'state', 'slug', 'created_at', 'publish_at', 'site_list'
+        'title', 'state', 'slug', 'created_at', 'publish_at', 'site_list'
     )
     list_filter = ('title', 'state', 'created_at', 'publish_at')
     search_fields = ('title',)
+
 
 class BannerSetAdmin(admin.ModelAdmin, SiteListAdminMixin):
     """Determine how the banner set is displayed in the admin."""
 
     list_display = ('slug', 'site_list')
 
+
 class GalleryAdmin(admin.ModelAdmin, SiteListAdminMixin):
     """Determined how the gallery is displayed in the admin."""
 
     list_display = ('slug', 'site_list')
+
+
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('content_type', 'object_id', 'series', 'number', 'state')
 
 admin.site.register(models.ContentModel, ContentModelAdmin)
 admin.site.register(models.ContentBlock, ContentModelAdmin)
@@ -46,3 +54,5 @@ admin.site.register(models.HTMLBannerSet, BannerSetAdmin)
 admin.site.register(models.ContentBlockSet, BannerSetAdmin)
 admin.site.register(models.GalleryImage)
 admin.site.register(models.Gallery, GalleryAdmin)
+admin.site.register(models.VersionSeries)
+admin.site.register(models.Version, VersionAdmin)
